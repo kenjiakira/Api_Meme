c97122aa078172716541d22eabf3bc5acb5811f1
Akira <kenjiakira2006@gmail.com>
2024-10-25 17:54:13 +0700

    first commit

---
+++ meme.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+const memes = [
+    "https://imgur.com/a/AozH1m5", 
+];
+
+const ensureJpgExtension = (url) => {
+  
+    if (!url.endsWith('.jpg')) {
+     
+        return `${url}.jpg`;
+    }
+    return url;
+};
+
+app.get('/', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * memes.length);
+    let memeUrl = memes[randomIndex];
+
+    // Đảm bảo URL có đuôi .jpg
+    memeUrl = ensureJpgExtension(memeUrl);
+
+    if (memeUrl) {
+        res.json({ memeUrl }); 
+    } else {
+        res.status(404).json({ message: 'Không tìm thấy meme nào.' });
+    }
+});
+
+module.exports = app;
