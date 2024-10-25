ef27d8f8dcb44726db1c70245eed32db8f4feec9
Akira <kenjiakira2006@gmail.com>
2024-10-25 18:36:29 +0700

    Add

--- api/meme.js
+++ api/meme.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 
 const app = express();
 
+const PORT = process.env.PORT || 9999;
+
 const memes = [
     "https://imgur.com/a/AozH1m5.jpg", 
 ];
 
 const ensureJpgExtension = (url) => {
-  
     if (!url.endsWith('.jpg')) {
-     
         return `${url}.jpg`;
     }
     return url;
@@ -20,7 +20,6 @@ app.get('/', (req, res) => {
     const randomIndex = Math.floor(Math.random() * memes.length);
     let memeUrl = memes[randomIndex];
 
-    // Đảm bảo URL có đuôi .jpg
     memeUrl = ensureJpgExtension(memeUrl);
 
     if (memeUrl) {
@@ -30,4 +29,6 @@ app.get('/', (req, res) => {
     }
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server đang chạy trên http://localhost:${PORT}`);
+});
